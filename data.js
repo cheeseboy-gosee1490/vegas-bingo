998e3812865ca92a89858a5b454e27e99a2a4ff3
cheeseboy-gosee1490 <83649561+cheeseboy-gosee1490@users.noreply.github.com>
2026-06-01 10:22:36 +0100

    Delete data.js

--- data.js
+++
@@ -1,5 +0,0 @@
-export const bingoItems = [
-"Elvis impersonator","Bride in a wedding dress","Bachelor party",
-"Bachelorette party","Someone drinking before 9am","Hear Mr Brightside",
-"Limousine","Influencer filming","Someone crying","Celebrity sighting"
-];
