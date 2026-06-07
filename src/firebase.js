1311ef7de995b642dc803582de7ab6adc6f20a20
cheeseboy-gosee1490 <83649561+cheeseboy-gosee1490@users.noreply.github.com>
2026-06-07 22:26:05 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCji6w7wWb8hAf_tX1m-J2351Yu3Zy-1Mg",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const storage = getStorage(app);
